9bfd22c4848ab2aaeff6440918173f6686e97104
Mirbek-W <81816961+Mirbek-W@users.noreply.github.com>
2022-03-23 09:45:04 +0300

    add css

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="style/css.css" rel="stylesheet" type="text/css">
+
+
+</head>
+
+
+<body>
+    <?php
+        echo "<h1>Проверка</h1> ";
+        
+        
+
+
+
+
+
+
+
+
+
+    ?>
+
+
+
+
+</body>
+
+
+
+
+
+</html>
